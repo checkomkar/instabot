123890515d6bc87bb94657c9bbb65b25625407c5
checkomkar <check.omkar@gmail.com>
2018-03-15 00:57:03 +0530

    auto_follows updated

--- auto_follow.py
+++ auto_follow.py
@@ -9,6 +9,9 @@ import os.path
 import logging
 import argparse
 import itertools
+import pickle
+from random import randint
+
 username = 'stra.tus'
 password = 'enter_sandmany3k'
 myId = '19335645'
@@ -47,18 +50,46 @@ except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
         print('ClientCookieExpiredError/ClientLoginRequiredError: {0!s}'.format(e))
 
 
-userinfo = api.username_info('adilmahfud_')
-userfollowers = api.user_followers(userinfo['user']['pk'])
-print json.dumps(userfollowers['users'], indent=4, sort_keys=True)
-print len(userfollowers['users'])
-'''users = userfollowers['users']
+users = ['eyebrows_tutorial', 'makeupgoals', 'eyelinertutorial', 'adilmahfud_', 'thehughjackman', 'tomcruise']
+
+follows = []
 count = 0
-for user in users:
-    status = api.friendships_create(user['pk'])
-    time.sleep(1)
+for user in users:    
+    userinfo = api.username_info(user)
+    userfollowers = api.user_followers(userinfo['user']['pk'])
+    # print json.dumps(userfollowers['users'], indent=4, sort_keys=True)
+    for idx, u in enumerate(userfollowers['users']):
+        follows.append(u['pk'])
+        
+        # print u['pk']
+    print len(userfollowers['users'])
+    '''users = userfollowers['users']
+    count = 0
+    for user in users:
+        status = api.friendships_create(user['pk'])
+        time.sleep(1)
+        count = count+1
+        print 'Followed username:', user['username']
+        print count
+        if count % 20 == 0:
+            time.sleep(300)
+    print 'Total followed:', count'''
+
+for idx, u in enumerate(follows):
+    status = api.friendships_create(u)
+    print json.dumps(status, indent=4, sort_keys=True)
+    time.sleep(randint(5, 9))
     count = count+1
-    print 'Followed username:', user['username']
+    # print 'Followed username:', u['username']
     print count
     if count % 20 == 0:
         time.sleep(300)
-print 'Total followed:', count'''
+    if idx+1 % 160 == 0:
+        time.sleep(60*60)
+
+with open('follows.json', 'wb') as fp:
+    pickle.dump(follows, fp)
+
+with open ('follows.json', 'rb') as fp:
+    itemlist = pickle.load(fp)
+# print itemlist
